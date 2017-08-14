9fc8dd5c41e7954c19fe7e91e7710c612e407d03
AkinaySau <akinaysau@gmail.com>
2017-08-14 15:01:14 +0300

    Добавлены обязательные плагины

--- tgm/tgm.php
+++ tgm/tgm.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * This file represents an example of the code that themes would use to register
+ * This file represents an example of the code that themes would use to
+ * register
  * the required plugins.
  *
  * It is expected that theme authors would copy and paste this code into their
  * functions.php file, and amend to suit.
  *
- * @see        http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ * @see        http://tgmpluginactivation.com/configuration/ for detailed
+ *             documentation.
  *
  * @package    TGM-Plugin-Activation
  * @subpackage Example
@@ -23,74 +25,88 @@
  * Depending on your implementation, you may want to change the include call:
  *
  * Parent Theme:
- * require_once get_template_directory() . '/path/to/class-tgm-plugin-activation.php';
+ * require_once get_template_directory() .
+ * '/path/to/class-tgm-plugin-activation.php';
  *
  * Child Theme:
- * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
+ * require_once get_stylesheet_directory() .
+ * '/path/to/class-tgm-plugin-activation.php';
  *
  * Plugin:
- * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
+ * require_once dirname( __FILE__ ) .
+ * '/path/to/class-tgm-plugin-activation.php';
  */
-require_once __DIR__ . '/class-tgm-plugin-activation.php';
+require_once __DIR__.'/class-tgm-plugin-activation.php';
 
-add_action( 'tgmpa_register', 'sau_tgm_register_required_plugins' );
+add_action('tgmpa_register', 'sau_tgm_register_required_plugins');
 
 /**
  * Register the required plugins for this theme.
  *
  * In this example, we register five plugins:
  * - one included with the TGMPA library
- * - two from an external source, one from an arbitrary source, one from a GitHub repository
- * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+ * - two from an external source, one from an arbitrary source, one from a
+ * GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the
+ * `is_callable` argument
  *
  * The variables passed to the `tgmpa()` function should be:
  * - an array of plugin arrays;
  * - optionally a configuration array.
- * If you are not changing anything in the configuration array, you can remove the array and remove the
- * variable from the function call: `tgmpa( $plugins );`.
- * In that case, the TGMPA default settings will be used.
+ * If you are not changing anything in the configuration array, you can remove
+ * the array and remove the variable from the function call: `tgmpa( $plugins
+ * );`. In that case, the TGMPA default settings will be used.
  *
- * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+ * This function is hooked into `tgmpa_register`, which is fired on the WP
+ * `init` action on priority 10.
  */
-function sau_tgm_register_required_plugins() {
-	$plugins = array(
-		array(
-			'name'     => 'Carbon Fields',
-			'slug'     => 'carbon-fields',
-			'required' => true,
-			'force_activation'   => true,
-		),
-		array(
-			'name'      => 'Sender',
-			'slug'      => 'sau_sender',
-			'source'    => 'https://github.com/AkinaySau/wp_sender/archive/master.zip',
-			'force_activation'   => true,
-		),
+function sau_tgm_register_required_plugins()
+{
+    $plugins = array(
+        array(
+            'name'             => 'Carbon Fields',
+            'slug'             => 'carbon-fields',
+            'required'         => true,
+            'force_activation' => true,
+        ),
+        array(
+            'name'             => 'The SEO Framework',
+            'slug'             => 'autodescription',
+            'required'         => true,
+            'force_activation' => true,
+        ),
+        array(
+            'name'             => 'SVG Support',
+            'slug'             => 'svg-support',
+            'required'         => true,
+            'force_activation' => true,
+        ),
+        array(
+            'name'             => 'WP Minify Fix',
+            'slug'             => 'wp-minify-fix',
+            'required'         => true,
+            'force_activation' => true,
+        ),
+        array(
+            'name'             => 'Sender',
+            'slug'             => 'sau_sender',
+            'source'           => 'https://github.com/AkinaySau/wp_sender/archive/master.zip',
+            'force_activation' => true,
+        ),
 
-	);
+    );
 
-	$config = array(
-		// Unique ID for hashing notices for multiple instances of TGMPA.
-		'id'           => 'tutmee_tgm',
-		// Default absolute path to bundled plugins.
-		'default_path' => '',
-		// Menu slug.
-		'menu'         => 'tgmpa-install-plugins',
-		// Parent menu slug.
-		'parent_slug'  => 'themes.php',
-		// Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'capability'   => 'edit_theme_options',
-		// Show admin notices or not.
-		'has_notices'  => true,
-		// If false, a user cannot dismiss the nag message.
-//		'dismissable'  => true,
-		// If 'dismissable' is false, this message will be output at top of nag.
-		'dismiss_msg'  => '',
-		// Automatically activate plugins after installation or not.
-		'is_automatic' => true,
-		// Message to output right before the plugins table.
-		'message'      => 'Tutmee Agency',
-	);
+    $config = array(
+        'id'           => 'tutmee_tgm',
+        'default_path' => '',
+        'menu'         => 'tgmpa-install-plugins',
+        'parent_slug'  => 'themes.php',
+        'capability'   => 'edit_theme_options',
+        'has_notices'  => true,
+        'dismiss_msg'  => '',
+        'is_automatic' => true,
+        'message'      => 'Tutmee Agency',
+    );
 
-	tgmpa( $plugins, $config );
+    tgmpa($plugins, $config);
 }
