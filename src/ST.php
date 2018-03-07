46c115b247cb764f0dd4150a385323e9b0d93127
sau <akinaysau@gmail.com>
2018-03-07 11:19:09 +0300

    Точка входа темы

---
+++ src/ST.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sau
+ * Date: 07.03.18
+ * Time: 11:16
+ */
+
+namespace Sau\WP\Theme\Source;
+
+
+use Sau\WP\Theme\InitTemplateClass;
+
+abstract class ST extends InitTemplateClass {
+	/**
+	 * For your code
+	 */
+	public static function initial () {
+
+	}
+}
