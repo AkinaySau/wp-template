92694737af0221ee5ee57f3caa7c70317487410d
sau <akinaysau@gmail.com>
2018-03-12 00:03:09 +0300

    SimpleSidebar.php

---
+++ src/Sidebar/SimpleSidebar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sau
+ * Date: 11.03.18
+ * Time: 23:28
+ */
+
+namespace Sau\WP\Theme\Source\Sidebar;
+
+
+use Sau\Lib\Sidebar;
+
+/**
+ * Override property for create custom sidebar
+ * @package Sau\WP\Theme\Source\Sidebar
+ */
+class SimpleSidebar extends Sidebar {
+
+}
