91daa56f4cad1395a01fe6b767bdb7c778f42fa9
sau <akinaysau@gmail.com>
2018-03-12 00:05:40 +0300

    Example Sidebar and Widget

--- src/ST.php
+++ src/ST.php
@@ -12,25 +12,26 @@ namespace Sau\WP\Theme\Source;
 use Sau\Lib\HF;
 use Sau\Lib\Theme;
 use Sau\WP\Theme\InitTemplateClass;
+use Sau\WP\Theme\Source\Carbon\FirstWidget;
+use Sau\WP\Theme\Source\Sidebar\SimpleSidebar;
 
 class ST extends InitTemplateClass {
-	public function customCode() {
+	protected $sidebars;
+
+	protected function customCode() {
 		Theme::addSupportTitleTag();
 		Theme::loadThemeTextdomain( THEME_LANG, get_stylesheet_directory() . DS . 'l10n' );
 
 		HF::addStyle( 'sau-style', get_stylesheet_directory_uri() . '/css/style.css' );
 		HF::addScript( 'sau-script', get_stylesheet_directory_uri() . '/js/bundle.js', [], false, true );
+		$this->carbon();
+
+		$this->sidebars[ SimpleSidebar::getId() ] = SimpleSidebar::init();
 	}
 
-	public function define() {
-		if ( ! defined( 'DS' ) ) {
-			define( 'DS', DIRECTORY_SEPARATOR );
-		}
-		if ( ! defined( 'THEME_LANG' ) ) {
-			define( 'THEME_LANG', 'sau_theme' );
-		}
-		parent::define();
-	}
 
+	protected function carbon() {
+		FirstWidget::init();
+	}
 
 }
